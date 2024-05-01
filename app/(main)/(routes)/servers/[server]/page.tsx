c5b5d3e5bf13405e7bf0289b78deaf732c414016
Rajshah1103 <rajshah11112003@gmail.com>
2024-05-01 10:14:15 +0530

    feat(Modals and Server Page): added the modals for creating the servers,login,logout,server page,database integration backend for creating server

---
+++ app/(main)/(routes)/servers/[server]/page.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+interface ServerIdPageProps {
+
+};
+
+const ServerIdPage : FC<ServerIdPageProps> = ({}) => {
+    return (
+        <div>
+            Server Page
+        </div>
+    )
+};
+
+export default ServerIdPage;

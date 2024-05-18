39c3e216b8d95a9815a578eced1b9d75b6ccb2cb
Rajshah1103 <rajshah11112003@gmail.com>
2024-05-19 00:41:42 +0530

    feat(Server Page): added the edit server functionality and edit the member settings modal and invite link functionality

--- app/(main)/(routes)/servers/layout.tsx
+++
@@ -1,9 +0,0 @@
-const ServerLayout = ({children}: {children: React.ReactNode}) => {
-    return (
-        <div>
-            {children}
-        </div>
-    )
-};
-
-export default ServerLayout
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,9 @@
 
 import { useEffect, useState } from "react";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "../modals/edit-server-modal";
+import { MembersModal } from "../modals/members-modal";
 
 
 export const ModalProvider = () => {
@@ -19,6 +22,9 @@ export const ModalProvider = () => {
     return (
         <>
             <CreateServerModal />
+            <InviteModal />
+            <EditServerModal/>
+            <MembersModal/>
         </>
     )
 }
